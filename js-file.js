886d1bad66e1cd993ec1bc69e3b1962bd47c5cfa
Kyaw Thu <evanch98@gmail.com>
2022-04-01 00:30:16 +0800

    Initial implementation of addBookToLibrary() function

--- js-file.js
+++ js-file.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const container = document.querySelector(".container");
+const formContainer = document.querySelector(".form-container");
+
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
@@ -8,25 +11,45 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
-
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    const addBook = new Book(title, author, pages, 'yes');
+    myLibrary.push(addBook);
 }
 
-const container = document.querySelector(".container");
-
-myLibrary[0] = new Book("AAA", "AAA", "123", "read it");
+// myLibrary[0] = new Book("AAA", "AAA", "123", "read it");
 
 function display() {
+    const items = document.querySelectorAll(".newItem");
+    items.forEach((item) => {
+        item.remove();
+    })
     for (let i = 0; i < myLibrary.length; i++) {
         const div1 = document.createElement('div');
-        div1.classList.add("item");
+        div1.classList.add("newItem");
         div1.textContent = myLibrary[i].title;
 
         const div2 = document.createElement('div');
-        div2.classList.add("item");
+        div2.classList.add("newItem");
         div2.textContent = myLibrary[i].author;
         container.appendChild(div1);
         container.appendChild(div2);
     }
 }
 
-display();
+const newBook = document.querySelector("#btn");
+newBook.addEventListener('click', () => {
+    formContainer.style.cssText = "opacity: 1";
+    display();
+});
+
+console.log(addBookToLibrary);
+
+const submit = document.querySelector("#submit");
+submit.addEventListener('click', () => {
+    addBookToLibrary();
+    formContainer.style.cssText = "opacity: 0"
+    console.log(myLibrary);
+});
+
