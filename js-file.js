51c4597d560095bc0028adc47cc471969bb89ecf
Kyaw Thu <evanch98@gmail.com>
2022-03-31 23:34:02 +0800

    Initial implementation of display function

--- js-file.js
+++ js-file.js
@@ -8,5 +8,25 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+const container = document.querySelector(".container");
+
+myLibrary[0] = new Book("AAA", "AAA", "123", "read it");
+
+function display() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const div1 = document.createElement('div');
+        div1.classList.add("item");
+        div1.textContent = myLibrary[i].title;
+
+        const div2 = document.createElement('div');
+        div2.classList.add("item");
+        div2.textContent = myLibrary[i].author;
+        container.appendChild(div1);
+        container.appendChild(div2);
+    }
+}
+
+display();
